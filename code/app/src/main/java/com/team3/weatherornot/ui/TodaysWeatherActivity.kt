e64e48655082196eb96b7f885ea6e019ad0e9061
menterzj3144 <47538306+menterzj3144@users.noreply.github.com>
2021-11-12 17:43:20 -0600

    Display api data on activity

--- code/app/src/main/java/com/team3/weatherornot/ui/TodaysWeatherActivity.kt
+++ code/app/src/main/java/com/team3/weatherornot/ui/TodaysWeatherActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.navigation.NavigationBarView
 import com.team3.weatherornot.api.Weather
@@ -11,6 +12,7 @@ import com.team3.weatherornot.myapplication.R
 import org.json.JSONObject
 
 class TodaysWeatherActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
+    var response: JSONObject = JSONObject()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,8 +20,17 @@ class TodaysWeatherActivity : AppCompatActivity(), NavigationBarView.OnItemSelec
 
         findViewById<NavigationBarView>(R.id.today_nav_view).setOnItemSelectedListener(this)
 
+        val tv = findViewById<TextView>(R.id.current_weather_temp)
+
+
+        //check if weather object exists. If it doesn't, make api call. If it does, use that instead
         val api = Weather(this)
-        val lambda = { json: JSONObject -> println(json.toString()) }
+        val lambda = {
+                json: JSONObject ->
+            response = json
+            //set text views then set weather object
+            tv.text = response.get("timezone").toString()
+        }
         api.executeRequest(44.811348, -91.498497, lambda)
     }
 
