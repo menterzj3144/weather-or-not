4fa1b4ee26eee781b7674026a66265813e8acf16
elainefranta <88861234+elainefranta@users.noreply.github.com>
2021-11-18 08:59:19 -0600

    Display API data WeeklyWeather
    Sunday display. Currently displays todays weather instead of sundays (need to fix).

--- code/app/src/main/java/com/team3/weatherornot/ui/WeeklyWeatherActivity.kt
+++ code/app/src/main/java/com/team3/weatherornot/ui/WeeklyWeatherActivity.kt
@@ -4,9 +4,14 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.navigation.NavigationBarView
 import com.team3.weatherornot.R
+import com.team3.weatherornot.api.APIManager
+import com.team3.weatherornot.api.WeatherAPIListener
+import com.team3.weatherornot.weather.Weather
 
 /**
  * The activity for the weekly weather view
@@ -20,7 +25,35 @@ class WeeklyWeatherActivity : AppCompatActivity(), NavigationBarView.OnItemSelec
         setContentView(R.layout.weekly_weather)
 
         findViewById<NavigationBarView>(R.id.weekly_nav_view).setOnItemSelectedListener(this)
+
+        APIManager.getInstance()!!.getWeatherForLocation(44.8113, -91.4985, object :
+            WeatherAPIListener<Weather> {
+            override fun getResult(result: Weather) {
+                populateSun(result)
+            }
+        })
     }
+
+
+    private fun populateSun(weather: Weather){
+        val imageView = findViewById<ImageView>(R.id.sunday_weather_img)
+        val minTemp = findViewById<TextView>(R.id.sun_mintemp)
+        val maxTemp = findViewById<TextView>(R.id.sun_maxtemp)
+        val precipSun = findViewById<TextView>(R.id.sun_precip)
+
+        //Fix day = 0 shows Today not Sunday
+        val sunday0 = weather.getSpecificDayWeather(0)
+
+        println(sunday0.weatherImgId)
+
+        val resId = resources.getIdentifier("icon_" + sunday0.weatherImgId, "drawable", packageName)
+        imageView.setImageResource(resId)
+
+        minTemp.text = (sunday0.minTemp.toString() + "°F")
+        maxTemp.text = (sunday0.maxTemp.toString() + "°F")
+        precipSun.text = (sunday0.precip.toString() + "%")
+    }
+
 
     /**
      * Switches to an activity based on which menu item was selected
